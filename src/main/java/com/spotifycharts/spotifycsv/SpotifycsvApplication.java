ab48ad192768ba1c9ac6af36d2e393c55a0dd8df
estopon <estopon@gmail.com>
2021-02-07 09:40:44 +0100

    First commit

---
+++ src/main/java/com/spotifycharts/spotifycsv/SpotifycsvApplication.java
@@ -0,0 +1,13 @@
+package com.spotifycharts.spotifycsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifycsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifycsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotifycharts/spotifycsv/SpotifycsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifycharts.spotifycsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifycsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
